861179a82af817aa121d0d225cbc9e86fbe8c802
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-07 02:34:24 +0530

    Add scrapers/logicimmo.py

---
+++ scrapers/logicimmo.py
@@ -0,0 +1,7 @@
+from .base_scraper import BaseScraper
+
+class LogicimmoScraper(BaseScraper):
+    def scrape(self):
+        self.logger.info("Scraping Logic-Immo...")
+        # TODO: Implement the scraping logic for Logic-Immo
+        pass
