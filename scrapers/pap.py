e856095874d55bcb0ae0f8aae95d58c1e2990d69
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-07 02:34:24 +0530

    Add scrapers/pap.py

---
+++ scrapers/pap.py
@@ -0,0 +1,7 @@
+from .base_scraper import BaseScraper
+
+class PapScraper(BaseScraper):
+    def scrape(self):
+        self.logger.info("Scraping PAP...")
+        # TODO: Implement the scraping logic for PAP
+        pass
