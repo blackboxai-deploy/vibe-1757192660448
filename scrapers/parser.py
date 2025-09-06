a61b837f55d955110a4169a357b8cb47b199b9c8
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-07 02:34:25 +0530

    Add scrapers/parser.py

---
+++ scrapers/parser.py
@@ -0,0 +1,7 @@
+def parse_apartment(listing):
+    # TODO: Implement the parsing logic for a single apartment listing
+    pass
+
+def standardize_data(data):
+    # TODO: Implement the data standardization logic
+    pass
