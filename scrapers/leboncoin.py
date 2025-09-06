e853c99f9dac2dcd7b1aeea908e8bf7a7ac1ce30
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-07 02:34:23 +0530

    Add scrapers/leboncoin.py

---
+++ scrapers/leboncoin.py
@@ -0,0 +1,7 @@
+from .base_scraper import BaseScraper
+
+class LeboncoinScraper(BaseScraper):
+    def scrape(self):
+        self.logger.info("Scraping Leboncoin...")
+        # TODO: Implement the scraping logic for Leboncoin
+        pass
