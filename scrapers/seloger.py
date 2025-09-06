8bbdb6403a827e589e6f7a78eda371120cda9bba
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-07 02:34:23 +0530

    Add scrapers/seloger.py

---
+++ scrapers/seloger.py
@@ -0,0 +1,7 @@
+from .base_scraper import BaseScraper
+
+class SelogerScraper(BaseScraper):
+    def scrape(self):
+        self.logger.info("Scraping SeLoger...")
+        # TODO: Implement the scraping logic for SeLoger
+        pass
